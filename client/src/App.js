41ade48845f89e49419dafe2a8520c077ac86bcf
Aayush Jain <imjainaayush@gmail.com>
2021-06-26 15:11:42 +0530

    project structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
+import Auth from './Components/Auth/Auth'
+import About from './Components/About/About'
+import Donate from './Components/Donate/Donate'
+import Home from './Components/Home/Home'
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Route path="donate">
+          <Donate />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
